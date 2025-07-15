bad96074a821bd34013a73a8209f2933fab90a47
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-07-15 18:52:15 +0530

    fetched the data and shown on UI accordingly

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
 
+  const [data ,setData] = useState([]);
+  const [input ,setInput] = useState('') ;
+
+  async function fetchData() 
+  {
+    const res = await fetch(`https://dummyjson.com/recipes/search?q=` + input);
+    const data = await res.json() ;
+    console.log(data.recipes)
+    setData(data.recipes)
+
+  }
+
+useEffect(()=> {
+    fetchData();
+
+
+},[input])
+
   return (
     <div>
-      <h1>Auto-Complete Search bar </h1>
+      <h1 className='heading'>Auto-Complete Search bar </h1>
+      <div className='input-div' >
+        <input type="text" placeholder='Enter the text' value={input} onChange={(e) => setInput(e.target.value)} className='input-text'  />
+      </div>
+     <div className='fetch-div'>
+      {
+        data.map((item)=> <p key={item.id}>{item.name}</p>)
+      }
+     </div>
     </div>
     
   )
