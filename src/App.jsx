80301ec77646e3add23320c0109f9bf48259c91e
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-07-15 18:55:56 +0530

    applied the onFocus and onBlur on inout

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,37 @@ import './App.css'
 
 function App() {
 
-  const [data ,setData] = useState([]);
-  const [input ,setInput] = useState('') ;
+  const [data, setData] = useState([]);
+  const [input, setInput] = useState('');
+  const [showresults, setShowresults] = useState(false);
 
-  async function fetchData() 
-  {
+  async function fetchData() {
     const res = await fetch(`https://dummyjson.com/recipes/search?q=` + input);
-    const data = await res.json() ;
+    const data = await res.json();
     console.log(data.recipes)
     setData(data.recipes)
 
   }
 
-useEffect(()=> {
+  useEffect(() => {
     fetchData();
 
 
-},[input])
+  }, [input])
 
   return (
     <div>
       <h1 className='heading'>Auto-Complete Search bar </h1>
       <div className='input-div' >
-        <input type="text" placeholder='Enter the text' value={input} onChange={(e) => setInput(e.target.value)} className='input-text'  />
+        <input type="text" placeholder='Enter the text' value={input} onChange={(e) => setInput(e.target.value)} className='input-text' onFocus={() => setShowresults(true)} onBlur={()=> setShowresults(false)} />
       </div>
-     <div className='fetch-div'>
-      {
-        data.map((item)=> <p key={item.id}>{item.name}</p>)
-      }
-     </div>
+      {showresults && <div className='fetch-div'>
+        {
+          data.map((item) => <p key={item.id}>{item.name}</p>)
+        }
+      </div>}
     </div>
-    
+
   )
 }
 
